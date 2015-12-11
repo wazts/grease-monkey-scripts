3fbdb53a5e1deca5906321316920f0c4606770cd
Kyle Wagner <tskazion@gmail.com>
2015-12-11 15:57:45 +0000

    Makes sure that new images get the same treatment.

--- rt-sponsor-chat-clickable-images.js
+++ rt-sponsor-chat-clickable-images.js
@@ -6,10 +6,17 @@
 // @author       Kyle Wagner
 // @match        http://roosterteeth.com/group/sponsors
 // @grant        none
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
 
 // Your code here...
-var img = $('div.chat-content').find('img');
-img.wrap($('<a></a>').attr({href: img.attr('src'), target: '_blank'}));
+
+function wrapImages (jNode) {
+    var img = jNode.find('img');
+    img.wrap($('<a></a>').attr({href: img.attr('src'), target: '_blank'}));
+}
+
+waitForKeyElements ('div.chat-content', wrapImages);
